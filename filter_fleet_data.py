570386957213617d3fbedec62f68b0683e8d8e91
Pigiby <pgb_2000@outlook.it>
2023-09-08 00:04:05 +0200

    commit

--- filter_fleet_data.py
+++ filter_fleet_data.py
@@ -1,20 +1,21 @@
 import csv
 
-def should_include_row(row):
-    return float(row[11]) == 0
 
-input_file = "flight_data.csv"
-output_file = "filtered_output.csv"
-
+input_file = "filtered_output.csv"
+output_file = "filtered_flights.csv"
+columns_to_remove = [17]
 with open(input_file, 'r', newline='') as csvfile_in, open(output_file, 'w', newline='') as csvfile_out:
     reader = csv.reader(csvfile_in)
     writer = csv.writer(csvfile_out)
 
     header = next(reader)
-    writer.writerow(header)
+    new_header =  [value for idx, value in enumerate(header) if idx not in columns_to_remove]
+    writer.writerow(new_header)
 
     for row in reader:
-        if should_include_row(row):
-            writer.writerow(row)
+        airline = row[6][1:len(row[6])-1]
+        row[6] = airline
+        new_row =  [value for idx, value in enumerate(row) if idx not in columns_to_remove]
+        writer.writerow(new_row)
 
 print("CSV file filtered and saved as", output_file)
