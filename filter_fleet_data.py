ece1ac595d355d9798386e6c7cf738cc66f212cf
mariadianacalugaru <104076211+mariadianacalugaru@users.noreply.github.com>
2023-09-08 10:01:49 +0200

    filter some fleet data

--- filter_fleet_data.py
+++ filter_fleet_data.py
@@ -3,7 +3,7 @@ import csv
 
 input_file = "filtered_output.csv"
 output_file = "filtered_flights.csv"
-columns_to_remove = [17]
+columns_to_remove = [11, 13, 17]
 with open(input_file, 'r', newline='') as csvfile_in, open(output_file, 'w', newline='') as csvfile_out:
     reader = csv.reader(csvfile_in)
     writer = csv.writer(csvfile_out)
